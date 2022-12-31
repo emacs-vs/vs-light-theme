988b649dcb9074719c7fd0ad3f289cf2995d7635
Jen-Chieh Shen <jcs090218@gmail.com>
2023-01-01 01:02:48 +0800

    Update copyright year

--- vs-light-theme.el
+++ vs-light-theme.el
@@ -1,6 +1,6 @@
 ;;; vs-light-theme.el --- Visual Studio IDE light theme
 
-;; Copyright (C) 2019-2022 , Jen-Chieh Shen
+;; Copyright (C) 2019-2023 , Jen-Chieh Shen
 
 ;; Author: Jen-Chieh Shen
 ;; URL: https://github.com/emacs-vs/vs-light-theme
