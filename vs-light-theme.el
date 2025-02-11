0c039a0efaabd2de96dd9c80f9a52c7e3adfa57c
JenChieh <jcs090218@gmail.com>
2025-02-10 17:23:38 -0800

    chore: Add spacing for dashboard headings

--- vs-light-theme.el
+++ vs-light-theme.el
@@ -191,7 +191,8 @@
 
  `(dashboard-text-banner       ((t :foreground "black")))
  `(dashboard-banner-logo-title ((t :foreground "#616161")))
- `(dashboard-heading           ((t :foreground "#727272")))
+ `(dashboard-heading           ((t :foreground "#727272"
+                                   :box (:line-width (-1 . 5) :color "#ffffff"))))
  `(dashboard-items-face        ((t :foreground "#1475B7")))
 
  `(yascroll:thumb-fringe    ((t :background "#C2C3C9" :foreground "#C2C3C9")))
